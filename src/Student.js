973b7f033008ab97b26c723872675ccc8d766172
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-07-14 00:05:18 -0400

    updated code

---
+++ src/Student.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Score from './Score';
+
+const Student = ({ student }) => {
+  return (
+    <div>
+      <h2>{student.name}</h2>
+      <p>{student.bio}</p>
+      {student.scores.map((score, index) => (
+        <Score key={index} score={score} />
+      ))}
+    </div>
+  );
+};
+
+export default Student;
